ba341b8a4413470701545702e85724c9e409b937
Jai Luthra <luthrajaiji@gmail.com>
2021-01-16 17:43:10 +0530

    final

---
+++ model/model.py
@@ -0,0 +1,74 @@
+from spacy.lang.en.stop_words import STOP_WORDS
+import json
+from gensim.models import Word2Vec
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+import nltk
+import wget
+nltk.download('stopwords','./nltk_data/')
+nltk.data.path.append('./nltk_data/')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+ignore_words = STOP_WORDS
+stop = stopwords.words('english')
+stop = set(stop)
+import zipfile
+for word in stop:
+  ignore_words.add(word)
+
+# wvUrl = "http://nlp.stanford.edu/data/glove.6B.zip"
+# zip_file = wget.download(wvUrl, './')
+# with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+#     zip_ref.extractall()
+
+
+glove_file = "./glove.6B/glove.6B.100d.txt"
+word2vec_glove_file = get_tmpfile("glove.6B.100d.word2vec.txt")
+glove2word2vec(glove_file,word2vec_glove_file)
+model = KeyedVectors.load_word2vec_format(word2vec_glove_file)
+word_vectors = model.wv
+data_file = open('QueriesAboutIITMandi.json').read()
+intents = json.loads(data_file)
+classno = 0
+
+def f(query):
+  fclass = None
+  ferr = 0
+  query = nltk.word_tokenize(query)
+  query = [lemmatizer.lemmatize(word.lower()) for word in query]
+  for intent in intents["QueriesAboutIITMandi"]:
+      err = 1
+      NotFound = 0
+      for qword in query:
+          if qword in ignore_words:
+                continue
+          if qword not in word_vectors.vocab:              
+            u = False
+            for pattern in intent["patterns"]:
+                w = nltk.word_tokenize(pattern)
+                if qword in w:
+                    u = True
+                    break
+            if not u:
+                NotFound +=1
+          else:
+              terr = 0.00001
+              for pattern in intent["patterns"]:
+                  w = nltk.word_tokenize(pattern)
+                  for word in w:
+                    if word in ignore_words:
+                        continue
+                    try:
+                      terr = max(terr,model.similarity(lemmatizer.lemmatize(qword),lemmatizer.lemmatize(word.lower())))
+                    except:
+                      pass
+              err *= terr
+      if err>ferr:
+          ferr = err
+          fclass = intent
+      return fclass
+
+def chatbot_response(query):
+    return f(query)
---
+++ model/old-main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+APP_NAME = "https://il-servitore.herokuapp.com/"
+TOKEN = "1568387409:AAHzWMtLieiNtzbQcCRyqXI9zsL3oV5BjiQ"
+import logging
+import os
+from googlesearch import search
+from model import chatbot_response
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', '8443'))
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! I am Il-servitore. I can help you with your queries. Send me a message or a command. Use /help to know usage.')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('You said Help!')
+
+
+"""Classify the message"""
+def category(message_text):
+    # return 1 # General
+    return 2 # IIT Mandi
+    # return 3 # Programming
+
+
+def reply(update, context):
+    """Reply to user message."""
+    answer = chatbot_response(update.message.text)
+    if category(update.message.text) != 1:
+        g = google_search(update.message.text, 1)
+        if g != "":
+            update.message.reply_text(f"{answer}\n\nIn case you want to know more, head over to {g}")
+        else:
+            update.message.reply_text(answer)
+    else:
+        update.message.reply_text(answer)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def google_search(query, max_results=1):
+
+    result=""
+
+    for j in search(query, tld="co.in", num=max_results, stop=max_results, pause=2): 
+        result += j + "\n"
+
+    return result
+
+
+def bot():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(
+        TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - reply to message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, reply))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
+    updater.bot.set_webhook(APP_NAME + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+bot()
